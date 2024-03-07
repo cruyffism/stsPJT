5aea6b2f3697367a89ad09d9e5f3ea6995d3aeac
alsrl <mkhong916@gmail.com>
2024-03-07 13:31:41 +0900

    service와 dao 구현 챕터 ch08_pjt_01

---
+++ ch08_pjt_01/src/main/java/com/company/hello/member/MemberController.java
@@ -0,0 +1,71 @@
+package com.company.hello.member;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MemberController {
+	
+	//의존 객체 직접 생성
+	//MemberService memberService = new  MemberService();
+	
+	//의존 객체 자동 주입
+	@Autowired
+	MemberService memberService;
+	
+	@RequestMapping("/signUp") //요청 url매핑
+	public String signUp() {
+		return "sign_up";
+	}
+	
+	@RequestMapping("/signIn") //요청 url매핑
+	public String signIn() {
+		return "sign_in";
+	}
+	
+	/*
+	 * @RequestMapping("/signUpConfirm") public String signUpConfirm() {
+	 * System.out.println("[MemberController] signUpConfirm()");
+	 * 
+	 * return null; }
+	 */
+	
+	/*
+	 * @RequestMapping("/signUpConfirm") public String signUpConfirm(@RequestParam
+	 * String m_id, //@RequestParam String m_pw,
+	 * 
+	 * @RequestParam int m_pw,
+	 * 
+	 * @RequestParam String m_mail,
+	 * 
+	 * @RequestParam String m_phone) {
+	 * 
+	 * System.out.println("[MemberController] signUpConfirm()");
+	 * 
+	 * System.out.println("m_id: " + m_id); System.out.println("m_pw: " + m_pw);
+	 * System.out.println("m_pw type: " + ((Object) m_pw).getClass().getName());
+	 * System.out.println("m_mail: " + m_mail); System.out.println("m_phone: " +
+	 * m_phone);
+	 * 
+	 * return null;
+	 * 
+	 * }
+	 */
+	
+	@RequestMapping("/signUpConfirm") 
+	public String signUpConfirm(MemberVo memberVo) {
+		System.out.println("[MemberController] signUpConfirm()");
+		
+		System.out.println("m_id: " + memberVo.getM_id());
+		System.out.println("m_pw: " + memberVo.getM_pw());
+		System.out.println("m_mail: " + memberVo.getM_mail());
+		System.out.println("m_phone: " + memberVo.getM_phone());
+		
+		memberService.signUpConfirm(memberVo);
+		return null;		
+	}
+	
+
+
+}
---
+++ ch08_pjt_01/src/main/webapp/WEB-INF/views/sign_up.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<h3>This page is SIGN_UP.</h3>
+
+	<form action="/hello/signUpConfirm"> <!-- RequestMapping : submit 클릭 시 넘어가는 서버 주소-->
+		ID : <input type="text" name="m_id"><br />
+		PW : <input type="password" name="m_pw"><br />
+		MAIL : <input type="text" name="m_mail"><br />
+		PHONE : <input type="text" name="m_phone"><br />
+		<input type="submit" value="SIGN UP">
+		<input type="reset" value="CANCEL">
+	</form>
+</body>
+</html>
