c3f15dadce65db5fae543dbc500156a0d63c958b
alsrl <mkhong916@gmail.com>
2024-03-07 17:29:01 +0900

    구민센터 회원가입 로그인 로그아웃 기능 예제

---
+++ MITMemberSystem/src/main/java/gumin/center/member/MemberController.java
@@ -0,0 +1,48 @@
+package gumin.center.member;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class MemberController {
+	
+	@Autowired
+	MemberService memberService;
+	
+	@RequestMapping("/signUp")
+	public String signUp() {
+		return "sign_up";
+	}
+	
+	@RequestMapping("/signIn")
+	public String signIn() {
+		return "sign_in";
+	}
+	
+	@RequestMapping("/signUpConfirm") 
+	public String signUpConfirm(MemberVo memberVo) {
+		System.out.println("[MemberController] signUpConfirm()");
+		
+		System.out.println("m_id: " + memberVo.getM_id());
+		System.out.println("m_pw: " + memberVo.getM_pw());
+		System.out.println("m_mail: " + memberVo.getM_mail());
+		System.out.println("m_phone: " + memberVo.getM_phone());
+		
+		memberService.signUpConfirm(memberVo);
+		return "sign_up_ok";
+		//return null;
+		
+	}
+	
+	@RequestMapping("/signInConfirm") 
+	public String signInConfirm(MemberVo memberVo) {
+		System.out.println("[MemberController] signInConfirm()");
+		
+		MemberVo signInedMember = memberService.signInConfirm(memberVo); //서비스 호출
+		
+		if (signInedMember != null) //로그인성공!
+			return "sign_in_ok";
+		else 
+			return "sign_in_ng";
+	}
+
+}
--- MITMemberSystem/src/main/java/gumin/center/member/MemberDao.java
+++ MITMemberSystem/src/main/java/gumin/center/member/MemberDao.java
@@ -8,13 +8,36 @@ import org.springframework.stereotype.Component;
 public class MemberDao {
 	
 		List<MemberVo> list = new ArrayList<MemberVo>(); 
-	
+		
+		/* MemberVo member1 = new MemberVo(); */
+		
 	public void insertMember (MemberVo memberVo) {
+		System.out.println("[MemberDao] insertMember()");
+		
 		System.out.println("m_id " + memberVo.getM_id());
 		System.out.println("m_pw: " + memberVo.getM_pw());
 		System.out.println("m_mail: " + memberVo.getM_mail());
 		System.out.println("m_phone: " + memberVo.getM_phone());
+		
+		list.add(memberVo);// 새로운 회원 정보 추가
+	}
+		
+	public MemberVo checkMember(MemberVo memberVo) {
+		
+		MemberVo signInedMember = null;
+		
+		for(MemberVo vo : list) {
+			if(vo.getM_id().equals(memberVo.getM_id())) {
+				signInedMember = vo;
+			} 
 
+		}
+		
+		if(signInedMember != null && memberVo.getM_pw().equals(signInedMember.getM_pw()))
+			return signInedMember;
+		else 
+			return null;
+		
 	}
 	
 }
